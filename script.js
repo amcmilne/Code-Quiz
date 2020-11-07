01c1723b08c1711fef05cc7107ba7328ca06ef78
amcmilne <amcmilne@gmail.com>
2020-11-07 17:35:26 -0500

    updated JS

--- script.js
+++ script.js
@@ -1,87 +1,118 @@
 // GIVEN I am taking a code quiz
 // WHEN I click the start button
-var startQuiz; 
-
-var timeLeft = 75;    
-var elem = document.getElementById("counter");
-  
-// 
 
 var timerId;
+var timeLeft;    
+var counterElement = document.getElementById("counter");
+
+var questionOneEl = document.getElementById('question-one');
+var questionTwoEl = document.getElementById('question-two'); 
+var questionThreeEl = document.getElementById('question-three');
+var finalResultsEl = document.getElementById('results');
 
 // timer starts     
 function countdown() {
-  //var timerId = setInterval(countdown, 1000);
-  if (timeLeft == -1) {
-    clearInterval(timerId);
+  if (timeLeft == 0) {
+    endGame();
   } else {
-    elem.innerHTML = timeLeft + ' seconds remaining';
+    counterElement.innerHTML = timeLeft + " seconds remaining";
     timeLeft--;
   }
 } 
 
-
-//function show/hide pass in element name show or hide
-
-
-document.getElementById("start-quiz").addEventListener("click", 
-    function (){
-      timerid = setInterval(countdown, 1000)
-
-    }
-);
- 
-
-
-//presented with a question
-var questionOneEl = document.getElementById("question-one");
-function quizTimer() {
-  if (timeLeft.value > 0) {
-    questionOneEl.style.visibility = 'visible';
-  } //start button is clicked, present a question
-}
-//questionOneEl.style.visibility = 'hidden';
-
-
-// WHEN I answer a question correctly
-
-
-
-
-
-// THEN I am presented with another question
-var questionTwoEl = document.getElementById('question-two');
-questionTwoEl.style.visibility = 'hidden';
-// questionTwoEl.style.visibility = 'visible';
-
-
-
-
-
-// WHEN I answer a question correctly
-
-// THEN I am presented with another question
-var questionThreeEl = document.getElementById('question-three');
-questionThreeEl.style.visibility = 'hidden';
-// questionThreeEl.style.visibility = 'visible';
-
-
 // WHEN I answer a question incorrectly
-
-
-
-
 // THEN time is subtracted from the clock
-  //function to subtract time from current time based on the user input
+function wrongAnswer() {
+  timeLeft = timeLeft - 10; 
+}
+
+function showHideElement(element, show) {
+  show === true ? element.style.display = "block" : element.style.display = "none"; 
+}
+
+function startGame() {
+  clearInterval(timerId);
+  timeLeft = 75;  
+  timerId = setInterval(countdown, 1000);
+  showHideElement(counterElement, true);
+  showHideElement(questionOneEl, true);
+  showHideElement(questionTwoEl, false);
+  showHideElement(questionThreeEl, false);
+
+  document.getElementById("final-results").innerHTML = "";
+
+  showHideElement(finalResultsEl, false);
+}
+
+function endGame() {
+  clearInterval(timerId);
+  showHideElement(questionOneEl, false);
+  showHideElement(questionTwoEl, false);
+  showHideElement(questionThreeEl, false);
+  showHideElement(counterElement, false);
+  
+  document.getElementById("final-results").innerHTML = "Your final score: " + timeLeft;
+
+  showHideElement(finalResultsEl, true);
+}
+
+function saveScore() {
+  sessionStorage.setItem(document.getElementById("Initials").value,timeLeft);
+}
+
+document.getElementById("start-quiz").addEventListener("click", startGame);
+document.getElementById("savescore").addEventListener("click", saveScore);
+
+
+// WHEN I answer a question correctly
+// THEN I am presented with another question
+var q1radios = document.querySelectorAll('input[type=radio][name="q1choice"]');
+for (var i = 0; i < q1radios.length; i++) {
+  q1radios[i].addEventListener("change", 
+    function() {
+        console.log(this.value);
+      if (this.value == "Alerts"){
+        showHideElement(questionOneEl, false);
+        showHideElement(questionTwoEl, true);
+      }
+      else {
+        wrongAnswer();
+      }
+    });
+} 
+
+// WHEN I answer a question correctly
+// THEN I am presented with another question
+var q2radios = document.querySelectorAll('input[type=radio][name="q2choice"]');
+for (var i = 0; i < q2radios.length; i++) {
+  q2radios[i].addEventListener("change", 
+    function() {
+        console.log(this.value);
+      if (this.value == "all of the above"){
+        showHideElement(questionTwoEl, false);
+        showHideElement(questionThreeEl, true);
+      }
+      else {
+        wrongAnswer();
+      }
+    });
+} 
+
+var q3radios = document.querySelectorAll('input[type=radio][name="q3choice"]');
+for (var i = 0; i < q3radios.length; i++) {
+  q3radios[i].addEventListener("change", 
+    function() {
+        console.log(this.value);
+      if (this.value == "console.log"){
+        endGame();
+      }
+      else {
+        wrongAnswer();
+      }
+    });
+} 
 
 // WHEN all questions are answered or the timer reaches 0
-  //if all questions answered || timer =0
-//   if (distance < 0) {
-//     clearInterval(x);
-//     document.getElementById("demo").innerHTML = "EXPIRED";
-//   }
-// }, 1000);
-
 // THEN the game is over
     //game over screen opens
 
